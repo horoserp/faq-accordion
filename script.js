5af9cc082e5f1ff9f672e0d8166d14d6ca1d7593
Robert Horosewski <horoserp@yahoo.com>
2024-11-13 03:06:45 -0500

    Update: Change icon when accordion is changed.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const buttons = document.getElementsByTagName("button");
 const paragraphs = document.getElementsByTagName("p");
+const images = document.getElementsByTagName("img");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
     for (let k = 0; k < paragraphs.length; k++) {
       if (k === i) {
         paragraphs[k].style.display = "block";
+        images[k + 1].src = "../images/icon-minus.svg";
       } else {
         paragraphs[k].style.display = "none";
+        images[k + 1].src = "../images/icon-plus.svg";
       }
     }
   });
